aa91f3b90b3f06806f0d8da22db4b1b4d1155f31
Gilad Ezer <gilad.ezer@post.runi.ac.il>
2025-12-29 20:26:04 +0200

    done

--- IntToBin.java
+++ IntToBin.java
@@ -6,15 +6,36 @@ public class IntToBin {
         System.out.println("Binary representation of " + x + " is: " + toBinary(x));
     }
 
+    private static String toBinaryPositive(int x) {
+        if (x == 0) {
+            return "";
+        }
+        return toBinaryPositive(x / 2) + (x % 2);
+    }
+
+    private static String toBinaryNegative(int x) {
+        if (x == 0) {
+            return "";
+        }
+        return toBinaryNegative(x >>> 1) + (x & 1);
+    }
+
+
     /** Returns the binary representation of the given integer, as a string of 0's and 1's. 5 = 101 */
     public static String toBinary(int x) {
-        if (x < 0) {
-            return "-" + toBinary(-x);
+
+        // Case 1: zero
+        if (x == 0) {
+            return "0";
         }
-        if (x < 2) {
-            return "" + x;
+
+        // Case 2: positive number
+        if (x > 0) {
+            return toBinaryPositive(x);
         }
-        return toBinary(x / 2) + (x % 2);
+
+        // Case 3: negative number (two’s complement)
+        return toBinaryNegative(x);
     }
   
  }
